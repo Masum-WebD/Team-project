69525d17d0b42229cbc05a95bcb146a752063b8d
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-07-09 12:41:19 +0600

    Added banner to app.js folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Banner from './Component/Banner';
 import Hero from './Component/Hero';
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Hero></Hero>
     </div>
   );
